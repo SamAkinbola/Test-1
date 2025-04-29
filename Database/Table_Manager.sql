ffd7336f2cf3abb91dfa87ceac7c0dc7bac556c9
SamAkinbola <122814732+SamAkinbola@users.noreply.github.com>
2025-04-29 19:04:47 +0100

    Add files via upload

--- Database/Table_Manager.sql
+++ Database/Table_Manager.sql
@@ -2,14 +2,15 @@
 CREATE OR REPLACE TABLE FINANCE.CREDIT.Manager (
     id INT,
     name VARCHAR(255),
+    gender VARCHAR(7),
     department VARCHAR(255),
     salary DECIMAL(10, 2)
 );
 
 -- Insert data into the "employees" table
 INSERT INTO FINANCE.CREDIT.Manager (id, name, department, salary) VALUES
-    (1, 'John Doe', 'Sales', 50000.00),
-    (2, 'Jane Smith', 'Marketing', 60000.00),
-    (3, 'Bob Johnson', 'Engineering', 75000.00),
-    (4, 'Alice Brown', 'Sales', 55000.00),
-    (5, 'Mike Davis', 'Engineering', 80000.00);
+    (1, 'John Doe', 'male', 'Sales', 50000.00),
+    (2, 'Jane Smith','female', 'Marketing', 60000.00),
+    (3, 'Bob Johnson', 'male', 'Engineering', 75000.00),
+    (4, 'Alice Brown', 'female', 'Sales', 55000.00),
+    (5, 'Mike Davis', 'male', 'Engineering', 80000.00);
