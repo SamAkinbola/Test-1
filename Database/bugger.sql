f7a2d062f35c83c89e2318be813cc01ec0169155
SamAkinbola <122814732+SamAkinbola@users.noreply.github.com>
2025-09-24 03:45:00 +0100

    Add files via upload

--- Database/bugger.sql
+++ Database/bugger.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE TABLE FINANCE.PUBLIC.Departmes (
+CREATE OR REPLACE TABLE FINANCE.PUBLIC.Departmenttts (
     EmployeeID INT PRIMARY KEY, -- Unique identifier for each employee
     FirstName VARCHAR(50),      -- Employee's first name
     LastName VARCHAR(50),       -- Employee's last name
